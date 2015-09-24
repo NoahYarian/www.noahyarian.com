372804e83eb6aaa7bfd00b96909b4334dff1d823
Noah Yarian <noah.yarian@gmail.com>
2015-09-24 14:42:19 -0500

    general layout sketched with example fullpage styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 // main.js
 
 console.log('testing!')
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff'],
+    anchors: ['welcome', 'about', 'projects', 'contact'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+  });
+});
